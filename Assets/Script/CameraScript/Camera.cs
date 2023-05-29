a339e055eadec4f80720752ba663581e43a7447e
407-07 <>
2023-05-29 12:19:38 +0900

    カメラの自動スクロール追加

---
+++ Assets/Script/CameraScript/Camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+     private float STEP = 2.0f;//移動速度
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(0, STEP * Time.deltaTime, 0);//上に移動
+    }
+}
--- Assets/Script/PlayerScript/Player.cs
+++ Assets/Script/PlayerScript/Player.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector3 plyPos;                             //プレイヤー座標
-    private Vector3 aimPos;                             //目標座標
-    private float moveDistance;                         //移動距離
-    private float startTime;
+    private GameObject[] targetObj;
+    private GameObject nearObj;                         //最も近いオブジェクト
+    private int objNum=1;
+    [SerializeField] GameObject camera;
+    private Vector3 cameraPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        startTime=Time.time;                            //開始時間をキャッシュ
-        plyPos=transform.position;                      //プレイヤーの位置をキャッシュ
-        aimPos=new Vector3(5,3,0);                     //目標座標設定
-        moveDistance=Vector3.Distance(plyPos,aimPos);       //目標までの距離を求める
-
+        targetObj=GameObject.FindGameObjectsWithTag("Warp");
+        camera = GameObject.Find("Main Camera");
     }
 
-    // Update is called once per frame
+    //更新処理
     void Update()
     {
-        float ipValue=(Time.time-startTime)/moveDistance;  //フレームの補間値を算出
-        transform.position=Vector3.Lerp(plyPos,aimPos,ipValue); //移動
+        //SearchObj();
+        if(transform.position.y>=targetObj[objNum].transform.position.y)
+        {
+            if(objNum!=0)
+            {
+                objNum--;
+            }
+        }
 
+
+        transform.position=Vector3.MoveTowards(transform.position,
+        targetObj[objNum].transform.position,5.0f*Time.deltaTime);
+
+        cameraPos = camera.transform.position;//カメラの座標取得
+        if(cameraPos.y > plyPos.y+2.0f)
+        {
+            Debug.Log("Game Over");
+        }
     }
 
     //当たり判定
@@ -34,6 +48,21 @@ public class Player : MonoBehaviour
         {
             Debug.Log("HIT");
             Destroy(gameObject);
+        }
+    }
+
+    void SearchObj()
+    {
+        targetObj=GameObject.FindGameObjectsWithTag("Warp");
+        foreach(GameObject obj in targetObj)
+        {
+            float nearDis=100.0f;
+            float objDis=Vector3.Distance(transform.position,obj.transform.position);
+            if(transform.position.y<=nearObj.transform.position.y&&nearDis>objDis)
+            {
+                nearObj=obj;
+                nearDis=objDis;
+            }
         }
     }
 }
